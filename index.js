4b48feb41751720bf09dc6ceff9bdea30f2f64d4
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2022-10-02 22:37:28 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import { config } from 'dotenv';
-import { Client } from 'discord.js';
-
-config();
-
-const client = new Client({ intents: ['Guilds', 'GuildMessages'] });
-const TOKEN = process.env.BOT_TOKEN;
-
-client.login(TOKEN);
