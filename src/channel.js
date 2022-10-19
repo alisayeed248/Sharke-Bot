f284f606db5140a7439e727040aa8694a5f8b233
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2022-10-18 20:34:21 -0400

    Create channel.js

---
+++ src/channel.js
@@ -0,0 +1,23 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+
+const channelsCommand = new SlashCommandBuilder()
+    .setName('channels')
+    .setDescription('channels cmd')
+    .addChannelOption((option) => 
+        option
+            .setName('channels')
+            .setDescription('channels')
+    )
+    .addBooleanOption((option) => 
+        option
+            .setName('deletemsgs')
+            .setDescription('Delete the messages')
+            .setRequired(true)
+    ).addIntegerOption((option) =>
+         option.setName('age').setDescription('Enter your age')
+    )
+    .addAttachmentOption((option) => 
+        option.setName('file').setDescription('file')
+    );
+
+export default channelsCommand.toJSON();  
