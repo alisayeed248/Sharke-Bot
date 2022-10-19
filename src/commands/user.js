a974e647ae68a3fb8220c4b3caf7eefef09e93d2
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2022-10-18 20:35:25 -0400

    Create user.js

---
+++ src/commands/user.js
@@ -0,0 +1,12 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+
+const usersCommand = new SlashCommandBuilder()
+    .setName('users')
+    .setDescription('users cmd')
+    .addUserOption((option) => 
+        option
+            .setName('user')
+            .setDescription('user')
+    );
+
+export default usersCommand.toJSON();  
